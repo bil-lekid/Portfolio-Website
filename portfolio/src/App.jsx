e87360e823adbbbcce3ebcdbe054e8dfc885838c
bil-lekid <billyhuangb5@gmail.com>
2024-01-21 18:06:07 +0700

    initial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Skills from './components/skills'
+import Work from './components/Work'
+import Contact from './components/Contact'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import {Button} from 'react-scroll'
+
+const Home = () => {
+  return (
+    <div name="home" className="bg-[#0a192f] w-full h-screen">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Billy Huang
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Video editor and Data scientist
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I'm a data science enthusiast and a Video Editor / Content Creator.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+          sunt?
+        </p>
+        <div>
+          <Button to="work" duration={500} smooth={true} className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
